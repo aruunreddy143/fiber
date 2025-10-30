379015f26c3a938d9cae816dc27fab986e9f2fa8
Piyush <testawsaws3@gmail.com>
2025-10-30 15:37:49 +0000

    Initial commit

---
+++ apps/fiberApp/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@fiber-x/fiberApp" />
+    </div>
+  );
+}
+
+export default App;
